d37211a8ff6c8d90587b821a289f7857f91edff3
Nate Grift <nategrift@Nates-MacBook-Pro.local>
2024-04-03 14:21:53 -0600

    chore: clean unused methods

--- lib/FileManager/FileManager.h
+++ lib/FileManager/FileManager.h
@@ -9,9 +9,6 @@ public:
     ~FileManager();
     bool writeData(const std::string& app, const std::string& filename, const std::string& data);
     std::string readData(const std::string& app, const std::string& filename);
-
-private:
-    bool ensureDirectoryExists(const std::string& path);
 };
 
 #endif // FILEMANAGER_H
